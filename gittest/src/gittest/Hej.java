a1c71c3fad58f58ae0b4e4937b76437fe74642b6
e000e <32869858+e000e@users.noreply.github.com>
2017-12-11 09:30:38 +0100

    Update Hej.java

--- gittest/src/gittest/Hej.java
+++ gittest/src/gittest/Hej.java
@@ -1,8 +1,8 @@
 package gittest;
-
+import javax.*;
 public class Hej {
 public static void main(String[] args) {
 	System.out.print("hej");
-	System.out.print("hej");
+	String s1=JOptionPane.showMessageDialog(null,"JARRÅÅ);
 }
 }
