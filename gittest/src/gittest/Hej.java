dc98f9f6313d148f2129aa7a0e49dcadfe6486d7
e000e <32869858+e000e@users.noreply.github.com>
2017-12-11 10:12:31 +0100

    update2

--- gittest/src/gittest/Hej.java
+++ gittest/src/gittest/Hej.java
@@ -1,11 +1,8 @@
 package gittest;
 public class Hej {
 public static void main(String[] args) {
-<<<<<<< HEAD
 
-=======
 	System.out.print("hej");
-	
->>>>>>> branch 'master' of https://github.com/e000e/test.git
+
 }
 }
