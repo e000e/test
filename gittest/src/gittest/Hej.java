c745aa1107435976f2930d8c9fff41183b548895
e000e <32869858+e000e@users.noreply.github.com>
2017-12-11 09:54:13 +0100

    test bygge

--- gittest/src/gittest/Hej.java
+++ gittest/src/gittest/Hej.java
@@ -1,8 +1,6 @@
 package gittest;
-import javax.*;
 public class Hej {
 public static void main(String[] args) {
 	System.out.print("hej");
-	String s1=JOptionPane.showMessageDialog(null,"JARRÅÅ);
 }
 }
